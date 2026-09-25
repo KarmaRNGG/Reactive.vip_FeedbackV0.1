029ad46e05a252d153819ee787bb8bb3afb78119
KarmaRNGG <ChattyCodexx@proton.me>
2026-09-25 23:43:04 +1000

    Assets folder was too big for upload (maps for geometry, wall bangs and Vis check) as a zip, so had to split it into multiple
    RUN THE MERGER BAT FIRST, EXTRACT THE ASSETS FOLDER. then run. Can delete all the split parts and zip after. If not the above won't work.
    
    Run normally as admin (right click) can be run normally, but I wouldn't trust it to be undetected or everything to function properly.
    
    This is the release of Reactive v0.1 for feedback :) Make sure everything stays in the same folder. Cfgs are more a base. HvH i use. But normally tweak and fk with it while playing. So, it's a reliable base. As mentioned, Hit chance is wrote myself from scratch (yes with references but I'm experimenting with new ways. And adding my own twists.
    
    I have beat multiple pay to cheats, reactive has bugs but is very "p" as the kids would say, just has some temperaments with hc%/Seed which you can turn off and wall bangs (sometimes I'm improving raycasting and pen detection most of the time it'll hit.) I'm improving again v0.1
    
    Not saying this will out beat anything not honestly, it won't but good cfg good gameplay and for a free v0.1undetect comp hvh based cheat it's damn good :) Hence why feedback is muchly needed as I'm designing it to be a semi-rage external. I'll improve/keep working on these things :) Started this late last night went to bed found it fkin stopped and errored while asleep. Hence the long wait since readme :)

---
+++ rejoin_assets.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Reassembling assets.zip...
+copy /b assets.zip.001 + assets.zip.002 + assets.zip.003 + assets.zip.004 + assets.zip.005 + assets.zip.006 + assets.zip.007 + assets.zip.008 + assets.zip.009 + assets.zip.010 + assets.zip.011 assets.zip
+echo Done! assets.zip is ready.
+pause
